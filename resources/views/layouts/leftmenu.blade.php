e94c3e55cebd7465cd5c5fef05fa2ca2f052814c
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2025-01-13 18:05:21 +0600

    css change for sidebar

--- resources/views/layouts/leftmenu.blade.php
+++ resources/views/layouts/leftmenu.blade.php
@@ -1,17 +1,48 @@
 <div id="menu" role="navigation">
-    <div style="text-align: -webkit-center;border-bottom: 2px solid #686868;margin-bottom: 13px;">
-        <a href="{{ URL::to('index') }}" class="logo navbar-brand mr-0">
-            @php
-                $setting = DB::table('sitesettings')->first();
-            @endphp
-            <h1 class="text-center" style="display: inline;"><img style="height: 60px;"  src="{{Storage::url((!empty($setting))?$setting->logo:'')}}" alt=""></h1>
-        </a>
-    </div>
-    <ul class="navigation list-unstyled" id="demo" style="height: 885px;display: flex;flex-direction: column;gap: 5px;">
+    
+    <style>
+.barrr {
+    background: rgb(255, 255, 255);
+    display: flex;
+    border-radius: 10px;
+    box-shadow: rgb(221, 221, 221) 0px 0px 4px 2px;
+    height: 82vh !important;
+    flex-direction: column;
+    padding: 24px 6px !important;
+    gap: 10px;
+    scrollbar-width: none;
+    overflow-y: scroll !important;
+    top: 55px;
+    margin: 5px;
+}
+    .barr2 {
+        background: rgb(255, 255, 255);
+        border-radius: 10px;
+        overflow: hidden;
+        box-shadow: 0px 0px 4px 2px #dddddd;
+        margin: auto 4px;
+        padding: 14px 7px 4px 9px !important;
+    }
+    .barr3 {
+        background: rgb(255, 255, 255)!important;
+    }
+    .barr4 {
+        margin-left: 24px;
+        background: rgb(255, 255, 255)!important;
+        overflow: hidden;
+        border-radius: 10px;
+        box-shadow: 0px 0px 4px 2px #dddddd;
+        margin-top: 5px;
+        display: flex;
+        flex-direction: column;
+        gap: 7px;
+    }
+    </style>
+    <ul class="navigation list-unstyled barrr" id="demo">
         <li><span class="close-icon d-xl-none d-lg-block"><img src="{{asset('img/images/input-disabled.png')}}"
                     alt="image missing"></span></li>
         <li {!! (Request::is('/*') ? 'class="active"' : '' ) !!}>
-            <a href="{{ URL::to('') }}" style="background: rgb(255, 255, 255);border-radius: 10px;overflow: hidden;">
+            <a href="{{ URL::to('') }}" class="barr2">
                 <span class="mm-text ">Dashboard</span>
                 <span class="menu-icon"><i class="im im-icon-Home"></i></span>
             </a>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -7,13 +7,13 @@
 
 
 <li {!! (Request::is('categories*')|| Request::is('items*') || Request::is('subCategories*') || Request::is('brands*') || Request::is('units*') ? 'class="menu-dropdown mm-active active"': "class='menu-dropdown'" ) !!}>
-    <a href="#"  style="background: rgb(255, 255, 255);border-radius: 10px;overflow: hidden;">
+    <a href="#"  class="barr2">
         <span class="mm-text ">Item Management</span>
         <span class="menu-icon "><i class="align-self-center fa-1x fas fa-diagnoses"></i></span>
         <span class="im im-icon-Arrow-Right imicon"></span>
     </a>
-    <ul class="sub-menu list-unstyled" style="margin-left: 24px;background: rgb(255, 255, 255);overflow: hidden;border-radius: 0px 0px 14px 14px;">
-        <li class="{!! (Request::is('items*') ? 'active' : '' ) !!}">
+    <ul class="sub-menu list-unstyled barr3">
+        <li class="barr4 {!! (Request::is('items*') ? 'active' : '' ) !!}">
             <a href="{{ route('items.index') }}">
                 <span class="mm-text ">Items</span>
             </a>
@@ -21,23 +21,23 @@
 
 
 
-        <li class="{!! (Request::is('categories*') ? 'active li_active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('categories*') ? 'active li_active' : '' ) !!}" >
             <a href="{{ route('categories.index') }}">
                 <span class="mm-text "> </span> Categories</span>
             </a>
         </li>
 
-        <li class="{!! (Request::is('subCategories*') ? 'active li_active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('subCategories*') ? 'active li_active' : '' ) !!}">
             <a href="{{ route('subCategories.index') }}">
                 <span class="mm-text ">Sub Categories</span>
             </a>
         </li>
-        <li class="{!! (Request::is('brands*') ? 'active li_active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('brands*') ? 'active li_active' : '' ) !!}">
             <a href="{{ route('brands.index') }}">
                 <span class="mm-text ">Brands</span>
             </a>
         </li>
-        <li class="{!! (Request::is('units*') ? 'active li_active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('units*') ? 'active li_active' : '' ) !!}">
             <a href="{{ route('units.index') }}">
                 <span class="mm-text ">Units</span>
             </a>
@@ -46,13 +46,13 @@
 </li>
 
 <li {!! (Request::is('pettyCashes*') ? 'class="menu-dropdown mm-active active"': "class='menu-dropdown'" ) !!}>
-    <a href="#"  style="background: rgb(255, 255, 255);border-radius: 10px;overflow: hidden;">
+    <a href="#"  class="barr2">
         <span class="mm-text ">Account</span>
         <span class="menu-icon "><i class="align-self-center fa-1x fas fa-diagnoses"></i></span>
         <span class="im im-icon-Arrow-Right imicon"></span>
     </a>
-    <ul class="sub-menu list-unstyled" style="margin-left: 24px;background: rgb(255, 255, 255);overflow: hidden;border-radius: 0px 0px 14px 14px;">
-        <li class="{!! (Request::is('pettyCashes*') ? 'active' : '' ) !!}">
+    <ul class="sub-menu list-unstyled barr3">
+        <li class="barr4 {!! (Request::is('pettyCashes*') ? 'active' : '' ) !!}">
             <a href="{{ route('pettyCashes.index') }}">
                 <span class="mm-text ">Petty Cash</span>
             </a>
@@ -61,43 +61,43 @@
 </li>
 
 <li {!! (Request::is('siteSettings*')|| Request::is('companies*') || Request::is('locations*') || Request::is('accountLedgers*') || Request::is('customers*') || Request::is('suppliers*') || Request::is('paymentMethods*')  ? 'class="menu-dropdown mm-active active"': "class='menu-dropdown'" ) !!}>
-    <a href="#"  style="background: rgb(255, 255, 255);border-radius: 10px;overflow: hidden;">
+    <a href="#"  class="barr2">
         <span class="mm-text ">Settings</span>
         <span class="menu-icon "><i class="align-self-center fa-1x fas fa-diagnoses"></i></span>
         <span class="im im-icon-Arrow-Right imicon"></span>
     </a>
-    <ul class="sub-menu list-unstyled" style="margin-left: 24px;background: rgb(255, 255, 255);overflow: hidden;border-radius: 0px 0px 14px 14px;">
-        <li class="{!! (Request::is('siteSettings*') ? 'active li_active' : '' ) !!}">
+    <ul class="sub-menu list-unstyled barr3">
+        <li class="barr4 {!! (Request::is('siteSettings*') ? 'active li_active' : '' ) !!}">
             <a href="{{ route('siteSettings.index') }}">
                 <span class="mm-text ">Site Settings</span>
             </a>
         </li>
-        <li class="{!! (Request::is('customers*') ? 'active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('customers*') ? 'active' : '' ) !!}">
             <a href="{{ route('customers.index') }}">
                 <span class="mm-text ">Customers</span>
             </a>
         </li>
-        <li class="{!! (Request::is('suppliers*') ? 'active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('suppliers*') ? 'active' : '' ) !!}">
             <a href="{{ route('suppliers.index') }}">
                 <span class="mm-text ">Suppliers</span>
             </a>
         </li>
-        <li class="{!! (Request::is('paymentMethods*') ? 'active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('paymentMethods*') ? 'active' : '' ) !!}">
             <a href="{{ route('paymentMethods.index') }}">
                 <span class="mm-text ">Payment Methods</span>
             </a>
         </li>
-        <li class="{!! (Request::is('accountLedgers*') ? 'active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('accountLedgers*') ? 'active' : '' ) !!}">
             <a href="{{ route('accountLedgers.index') }}">
                 <span class="mm-text ">Account Ledgers</span>
             </a>
         </li>
-        <li class="{!! (Request::is('locations*') ? 'active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('locations*') ? 'active' : '' ) !!}">
             <a href="{{ route('locations.index') }}">
                 <span class="mm-text ">Locations</span>
             </a>
         </li>
-        <li class="{!! (Request::is('companies*') ? 'active' : '' ) !!}">
+        <li class="barr4 {!! (Request::is('companies*') ? 'active' : '' ) !!}">
             <a href="{{ route('companies.index') }}">
                 <span class="mm-text ">Companies</span>
             </a>
