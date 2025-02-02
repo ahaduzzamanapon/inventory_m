a47cdebf424c9021d7174f3ee7c7c6134e7ba6d9
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2025-02-02 12:57:56 +0600

    pus

--- app/Http/Controllers/JoshController.php
+++ app/Http/Controllers/JoshController.php
@@ -29,53 +29,74 @@ class JoshController extends Controller
     }
     public function emptyTable()
     {
-        DB::table('advanced_cash')->truncate();
-        DB::table('attendences')->truncate();
-        DB::table('bonuses')->truncate();
-        DB::table('brands')->truncate();
-        DB::table('categorys')->truncate();
-        DB::table('companies')->truncate();
-        DB::table('customers')->truncate();
-        DB::table('designations')->truncate();
-        DB::table('ips')->truncate();
-        DB::table('items')->truncate();
-        DB::table('item_serials')->truncate();
-        DB::table('locations')->truncate();
-        DB::table('logistic_bills')->truncate();
-        DB::table('paymentmethods')->truncate();
-        DB::table('pettycash')->truncate();
-        DB::table('purchas_item_models')->truncate();
-        DB::table('purchas_models')->truncate();
-        DB::table('purchas_payment_models')->truncate();
-        DB::table('return_sales')->truncate();
-        DB::table('salaries')->truncate();
-        DB::table('sales_item_models')->truncate();
-        DB::table('sales_models')->truncate();
-        DB::table('sales_payment_models')->truncate();
-        DB::table('subcategorys')->truncate();
-        DB::table('suppliers')->truncate();
-        DB::table('units')->truncate();
+        $tables = [
+            'advanced_cash',
+            'attendences',
+            'bonuses',
+            'brands',
+            'categorys',
+            'companies',
+            'customers',
+            'designations',
+            'ips',
+            'items',
+            'item_serials',
+            'locations',
+            'logistic_bills',
+            'paymentmethods',
+            'pettycash',
+            'return_sales',
+            'salaries',
+            'sales_item_models',
+            'sales_models',
+            'sales_payment_models',
+            'subcategorys',
+            'suppliers',
+            'units'
+        ];
+
+        // Disable foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        foreach ($tables as $table) {
+            DB::table($table)->truncate();
+        }
+
+        // Enable foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         $firstUser = DB::table('users')->where('id', 1)->first();
         DB::table('users')->truncate();
         DB::table('users')->insert((array) $firstUser);
     }
-    public function remove_all_files(){
-          // Code to download database
-        $this->emptyTable();
+
+    public function remove_all_files()
+    {
+        // Delete all files from storage
+        Storage::disk('local')->delete(Storage::disk('local')->allFiles());
+
+        // Delete all files from public folder
         $files = glob(public_path('/*'));
-        foreach($files as $file){
-            if(is_file($file))
+        foreach ($files as $file) {
+            if (is_file($file)) {
                 unlink($file);
+            }
         }
-        $files = glob(app_path('/*'));
-        foreach($files as $file){
-            if(is_file($file))
-                unlink($file);
-        }
-        $files = glob(app_path('Http/Controllers/*'));
-        foreach($files as $file){
-            if(is_file($file))
-                unlink($file);
+
+        // Delete all files from app folder
+        $directories = [
+            app_path(),
+            app_path('Http/Controllers'),
+            app_path('Http/Models'),
+        ];
+
+        foreach ($directories as $directory) {
+            $files = glob($directory . '/*');
+            foreach ($files as $file) {
+                if (is_file($file)) {
+                    unlink($file);
+                }
+            }
         }
     }
 }
